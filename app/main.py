b13514e87bbd505d36d429294dd31efc74c8a9cb
Brian Hockett <mgh2xx@virginia.edu>
2024-11-13 12:34:18 -0500

    Updated main.py for the diy-spotify data project

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-
+import mysql.connector
 from fastapi import FastAPI
 from typing import Optional
 from pydantic import BaseModel
@@ -7,6 +7,49 @@ import json
 import os
 
 app = FastAPI()
+from fastapi.middleware.cors import CORSMiddleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+DBHOST = "ds2022.cqee4iwdcaph.us-east-1.rds.amazonaws.com"
+DBUSER = "admin"
+DBPASS = os.getenv('DATABASE_PASS')
+DB = "mgh2xx"
+
+db = mysql.connector.connect(user=DBUSER, host=DBHOST, password=DBPASS, database=DB, autocommit = True)
+cur=db.cursor()
+
+@app.get('/genres')
+def get_genres():
+    query = "SELECT * FROM genres ORDER BY genreid;"
+    try:    
+        cur.execute(query)
+        headers=[x[0] for x in cur.description]
+        results = cur.fetchall()
+        json_data=[]
+        for result in results:
+            json_data.append(dict(zip(headers,result)))
+        return(json_data)
+    except Error as e:
+        return {"Error": "MySQL Error: " + str(e)}
+
+@app.get('/songs')
+def get_songs():
+    query = "SELECT title, album, artist, year, file, image, genres.genre FROM songs LEFT JOIN genres ON songs.genre=genres.genreid;"
+    try:    
+        cur.execute(query)
+        headers=[x[0] for x in cur.description]
+        results = cur.fetchall()
+        json_data=[]
+        for result in results:
+            json_data.append(dict(zip(headers,result)))
+        return(json_data)
+    except Error as e:
+        return {"Error": "MySQL Error: " + str(e)}
 
 @app.get("/")  # zone apex
 def zone_apex():
@@ -31,3 +74,6 @@ def subtract(f: int, g: int):
 @app.get("/diff_squares/{h}/{i}")
 def diff_squares(h :int, i: int):
     return {"difference of squares": (h**2) - (i**2)}
+
+
+
