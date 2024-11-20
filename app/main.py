8edd2919fbc8e967dbe781189c32ed645942296b
Brian Hockett <mgh2xx@virginia.edu>
2024-11-20 13:28:12 -0500

    Changes to main.py - moved db connection inside methods

--- app/main.py
+++ app/main.py
@@ -20,11 +20,11 @@ DBUSER = "admin"
 DBPASS = os.getenv('DATABASE_PASS')
 DB = "mgh2xx"
 
-db = mysql.connector.connect(user=DBUSER, host=DBHOST, password=DBPASS, database=DB, autocommit = True)
-cur=db.cursor()
 
 @app.get('/genres')
 def get_genres():
+    db = mysql.connector.connect(user=DBUSER, host=DBHOST, password=DBPASS, database=DB, autocommit = True)
+    cur=db.cursor()
     query = "SELECT * FROM genres ORDER BY genreid;"
     try:    
         cur.execute(query)
@@ -33,12 +33,18 @@ def get_genres():
         json_data=[]
         for result in results:
             json_data.append(dict(zip(headers,result)))
+        cur.close()
+        db.close()
         return(json_data)
     except Error as e:
+        cur.close()
+        db.close()
         return {"Error": "MySQL Error: " + str(e)}
 
 @app.get('/songs')
 def get_songs():
+    db = mysql.connector.connect(user=DBUSER, host=DBHOST, password=DBPASS, database=DB, autocommit = True)
+    cur=db.cursor()
     query = "SELECT title, album, artist, year, file, image, genres.genre FROM songs LEFT JOIN genres ON songs.genre=genres.genreid;"
     try:    
         cur.execute(query)
@@ -47,8 +53,12 @@ def get_songs():
         json_data=[]
         for result in results:
             json_data.append(dict(zip(headers,result)))
+        cur.close()
+        db.close()
         return(json_data)
     except Error as e:
+        cur.close()
+        db.close()
         return {"Error": "MySQL Error: " + str(e)}
 
 @app.get("/")  # zone apex
